c4d55ae50cd3b0da966384611cb4a385b645eaa5
Mikhail Mazurovskiy <mazurovsky.mike@gmail.com>
2021-11-14 19:44:18 +0300

    - changed response sealed

--- lib/client/response_sealed.dart
+++ lib/client/response_sealed.dart
@@ -5,6 +5,6 @@ part 'response_sealed.freezed.dart';
 
 @freezed
 class ResponseSealed<DATA> with _$ResponseSealed {
-  const factory ResponseSealed.success(DATA data) = SuccessResponse;
+  const factory ResponseSealed.success(DATA data) = SuccessResponse<DATA>;
   const factory ResponseSealed.failure(Failure failure) = FailureResponse;
 }
