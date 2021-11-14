9cdc18cc777121b2769dba5c5b0ae78b9fb65873
Mikhail Mazurovskiy <mazurovsky.mike@gmail.com>
2021-11-14 20:00:42 +0300

    - fixed response sealed freezed union class with generic type

--- lib/client/response_sealed.dart
+++ lib/client/response_sealed.dart
@@ -4,7 +4,7 @@ import '../errors/failures.dart';
 part 'response_sealed.freezed.dart';
 
 @freezed
-class ResponseSealed<DATA> with _$ResponseSealed {
+class ResponseSealed<DATA> with _$ResponseSealed<DATA> {
   const factory ResponseSealed.success(DATA data) = SuccessResponse<DATA>;
-  const factory ResponseSealed.failure(Failure failure) = FailureResponse;
+  const factory ResponseSealed.failure(Failure failure) = FailureResponse<DATA>;
 }
