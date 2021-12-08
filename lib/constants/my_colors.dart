bb00ed5d7b4f3ee9cf4567010c06170737a8bf7e
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2021-12-08 17:59:35 +0300

    - edited colors

--- lib/constants/my_colors.dart
+++ lib/constants/my_colors.dart
@@ -11,9 +11,10 @@ class MyColors {
   static const Color forEventCardBrighter = Color.fromRGBO(73, 79, 90, 1);
   static const Color forInactiveStuff = Color.fromRGBO(166, 168, 169, 1);
   static const Color forInactiveStuffDarker = Color.fromRGBO(50, 51, 52, 1);
-  static const Color forPositiveWeeklyRating = Color.fromRGBO(124, 223, 100, 1);
-  static const Color forNegativeWeeklyRating = Color.fromRGBO(252, 119, 83, 1);
+  static const Color forPositiveWeeklyRating = Color.fromRGBO(155, 240, 160, 1);
+  static const Color forNegativeWeeklyRating = Color.fromRGBO(240, 155, 155, 1);
   static const Color forDividers = Color.fromRGBO(198, 199, 200, 1);
+  static const Color forErrors = Color.fromRGBO(129, 0, 0, 1);
   static const forInactiveBottomBarIcons = main;
   static const forActiveBottomBarIcons = accent;
   static Color forSearchContainer = Colors.grey.withOpacity(0.2);
