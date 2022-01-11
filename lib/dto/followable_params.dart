2d55dfb160a75ddfbab9d9a3fbb40fb9b7c2531f
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2022-01-11 18:38:40 +0300

    - converted followable variables to change notifier

--- lib/dto/followable_params.dart
+++ lib/dto/followable_params.dart
@@ -1,27 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:the_postraves_package/dto/followable_type.dart';
 
 part 'followable_params.freezed.dart';
-
-@freezed
-class FollowableVariables with _$FollowableVariables {
-  const FollowableVariables._();
-
-  const factory FollowableVariables({
-    required int overallFollowers,
-    required int weeklyFollowers,
-    required bool isFollowed,
-  }) = _FollowableVariables;
-
-  factory FollowableVariables.empty() 
-  {
-    return const FollowableVariables(
-      overallFollowers: 0,
-      weeklyFollowers: 0,
-      isFollowed: false,
-    );
-  }
-}
 
 @freezed
 class FollowableId with _$FollowableId {
@@ -29,4 +10,40 @@ class FollowableId with _$FollowableId {
     required int id,
     required FollowableType type,
   }) = _FollowableId;
+}
+
+class FollowableVariables with ChangeNotifier {
+  late int _overallFollowers;
+  late int _weeklyFollowers;
+  late bool _isFollowed;
+
+  FollowableVariables({
+    required int overallFollowers,
+    required int weeklyFollowers,
+    required bool isFollowed,
+  }) {
+    _overallFollowers = overallFollowers;
+    _weeklyFollowers = weeklyFollowers;
+    _isFollowed = isFollowed;
+  }
+
+  factory FollowableVariables.empty() {
+    return FollowableVariables(
+      overallFollowers: 0,
+      weeklyFollowers: 0,
+      isFollowed: false,
+    );
+  }
+
+  void toggleFollow() {
+    if (_isFollowed) {
+      _overallFollowers--;
+      _weeklyFollowers--;
+    } else {
+      _overallFollowers++;
+      _weeklyFollowers++;
+    }
+    _isFollowed = !_isFollowed;
+    notifyListeners();
+  }
 }
